1234b6846c64cc07b4298d4ceaad48c3b49af3a9
Zaid Shaikh <152058321+Zaiddd96@users.noreply.github.com>
2024-03-10 07:24:01 +0530

    Beginner!
    Of course! This is a basic Python body mass index calculator. BMI is a weight-and-height-based indicator of body fat. It's frequently used to determine if a person is obese, overweight, healthy, or underweight.

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,6 +1,6 @@
-# Enter your height in meters e.g., 1.55
+# Enter your height in meters
 height = float(input())
-# Enter your weight in kilograms e.g., 72
+# Enter your weight in kilograms
 weight = int(input())
 bmi = weight / height ** 2
 if bmi < 18.5:
